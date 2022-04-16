fea742ecce2c6e4e298630a58dadcdba3371330c
GautamTechnologies <100987552+GautamTechnologies@users.noreply.github.com>
2022-04-16 11:26:05 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to AXOCON LABS" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming soon1
         </p>
       </main>
 
